061ad79e85763ea5abd9c5dfbe360fcb3a027674
Juan Camilo Bages <jcbages@outlook.com>
2015-09-17 18:38:47 -0500

    database with postgres

--- tbcSite/settings.py
+++ tbcSite/settings.py
@@ -90,10 +90,21 @@ WSGI_APPLICATION = 'tbcSite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'admin',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '6432'
     }
 }
 
